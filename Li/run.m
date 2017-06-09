8bd9662a26b93b2fd1739be8d4e7c07cdc578da8
Kavendish <jingzhe.chen@gmail.com>
2017-06-09 15:29:59 +0800

    new

--- Li/run.m
+++ Li/run.m
@@ -1,9 +1,11 @@
 Rs = [[0,0];[0,1];[0,-1];[1,0];[-1,0]];
 H = zeros(8, 8);
+k = [0,0.2];
+
 for i = 1:5
     R = Rs(i,:);
     h = get_realspace_hamiltonian(R);
-    H = H + h;
+    H = H + h*exp(1.j*k*R'*2*pi);
 end
 eta = 1e-6;
 G = inv(1.j*eta*eye(8)-H);
